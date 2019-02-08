905ca1ae1420648592456fa1ccba134cd2590e0f
fjw <1281043316@qq.com>
2019-02-08 12:13:48 +0800

    新增短信微服务

---
+++ ly-user/ly-user-service/src/main/java/com/leyou/user/web/UserController.java
@@ -0,0 +1,23 @@
+package com.leyou.user.web;
+
+import com.leyou.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("check/{data}/{type}")
+    public ResponseEntity<Boolean> checkUserData(@PathVariable("data") String data, @PathVariable("type") Integer type) {
+        Boolean boo = this.userService.checkData(data, type);
+        if (boo == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(boo);
+    }
+}
